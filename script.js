0771b123dd476c76cb6be3601e52ff7b0f1d9895
wesleyhuang23 <wesleyhuang23@gmail.com>
2017-03-10 21:19:13 -0800

    sorint images by filename

---
+++ script.js
@@ -0,0 +1,25 @@
+new DroneDeploy({version: 1}).then(function(api){
+  console.log('DroneDeploy Api: ', api);
+  api.Plans.getCurrentlyViewed().then(function(plan){
+    console.log(plan);
+    api.Images.get(plan.id, {width: 450, height: 450}).then(function(arrayOfImageInformation){ 
+    arrayOfImageInformation.sort(function(a, b) {
+      if (a.data.filename < b.data.filename) {
+        return -1;
+      }
+      if (a.data.filename > b.data.filename) {
+        return 1;
+      }
+
+      // names must be equal
+      return 0;
+    });
+    console.log(arrayOfImageInformation) 
+  });
+
+});
+  
+});
+
+
+
